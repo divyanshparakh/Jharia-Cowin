8c1768c964fd213abd7530b9a2585532391f945f
divz11 <itsurdivyansh@gmail.com>
2021-05-25 11:46:08 +0530

    Requirements Updated

--- bot.py
+++ bot.py
@@ -39,6 +39,7 @@ while True:
     port = os.getenv('PORT', default=8000)
 
     response = requests.get(CowinAPI, PARAMS, headers={
+        "Host": 'cdn-api.co-vin.in',
         "Accept": "application/json",
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     })
